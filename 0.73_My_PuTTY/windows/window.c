ae5b171b200f0211483cda473906a2b074a18c51
Adrian Kocis <adrian.kocis@gmail.com>
2020-01-17 11:09:53 +0000

    Comment out _WTS_VIRTUAL_CLASS (done by make process)

--- 0.73_My_PuTTY/windows/window.c
+++ 0.73_My_PuTTY/windows/window.c
@@ -326,7 +326,7 @@ int WINAPI Agent_WinMain(HINSTANCE inst, HINSTANCE prev, LPSTR cmdline, int show
 
 #endif
 #ifdef MOD_WTS
-typedef enum _WTS_VIRTUAL_CLASS { WTSVirtualClientData, WTSVirtualFileHandle } WTS_VIRTUAL_CLASS; 		// WTS_VIRTUAL_CLASS n'est pas défini dans le fichier wtsapi32.h !!!
+// typedef enum _WTS_VIRTUAL_CLASS { WTSVirtualClientData, WTSVirtualFileHandle } WTS_VIRTUAL_CLASS; 		// WTS_VIRTUAL_CLASS n'est pas défini dans le fichier wtsapi32.h !!!
 #include <wtsapi32.h>
 #endif
 #if (defined MOD_BACKGROUNDIMAGE) && (!defined FDJ)
